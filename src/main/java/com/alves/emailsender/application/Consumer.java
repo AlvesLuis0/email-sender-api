6f9463941aa111d8f8af2504d1a6ebc55ca19204
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-01-27 19:53:29 -0300

    Consumer implemented

---
+++ src/main/java/com/alves/emailsender/application/Consumer.java
@@ -0,0 +1,32 @@
+package com.alves.emailsender.application;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.alves.emailsender.adapters.EmailSenderGateway;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class Consumer {
+
+  private final EmailSenderGateway emailSenderGateway;
+  private final ObjectMapper mapper;
+
+  @RabbitListener(queues = "email-queue")
+  public void sendEmail(@Payload Message<String> message) throws JsonMappingException, JsonProcessingException {
+    var json = mapper.readTree(message.getPayload());
+    emailSenderGateway.sendEmail(
+      json.get("to").asText(),
+      json.get("subject").asText(),
+      json.get("text").asText()
+    );
+  }
+  
+}
--- src/main/java/com/alves/emailsender/infra/config/JavaMailSenderConfig.java
+++ src/main/java/com/alves/emailsender/infra/config/JavaMailSenderConfig.java
@@ -33,7 +33,6 @@ public class JavaMailSenderConfig {
     props.put("mail.transport.protocol", "smtp");
     props.put("mail.smtp.auth", "true");
     props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.debug", "true");
     return mailSender;
   }
   
