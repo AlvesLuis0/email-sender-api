aa61d5e2209945aca4dd198d18c6e4b1067fee5b
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-01-27 20:03:12 -0300

    automatic queue parametrization

--- src/main/java/com/alves/emailsender/infra/config/RabbitMQConfig.java
+++ src/main/java/com/alves/emailsender/infra/config/RabbitMQConfig.java
@@ -1,6 +1,11 @@
 package com.alves.emailsender.infra.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Getter;
@@ -12,7 +17,23 @@ import lombok.Setter;
 @Configuration
 public class RabbitMQConfig {
 
+  private String queueName;
   private String exchangeName;
   private String routingKey;
+
+  @Bean
+  public Queue getQueue() {
+    return new Queue(queueName, true);
+  } 
+
+  @Bean
+  public TopicExchange getExchange() {
+    return new TopicExchange(exchangeName);
+  }
+
+  @Bean
+  public Binding getBinding(Queue queue, TopicExchange exchange) {
+    return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+  }
   
 }
