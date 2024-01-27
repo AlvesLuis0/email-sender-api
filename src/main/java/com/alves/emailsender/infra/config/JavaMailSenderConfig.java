50b94f44fbf8f50154320e145779ab087c681dd5
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-01-27 19:22:50 -0300

    JavaMail configuration added

---
+++ src/main/java/com/alves/emailsender/infra/config/JavaMailSenderConfig.java
@@ -0,0 +1,40 @@
+package com.alves.emailsender.infra.config;
+
+import java.util.Properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ConfigurationProperties("mail")
+@Configuration
+public class JavaMailSenderConfig {
+
+  private String host;
+  private int port;
+  private String username;
+  private String password;
+
+  @Bean
+  public JavaMailSender getJavaMailSender() {
+    var mailSender = new JavaMailSenderImpl();
+    mailSender.setHost(host);
+    mailSender.setPort(port);
+    mailSender.setUsername(username);
+    mailSender.setPassword(password);
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+    return mailSender;
+  }
+  
+}
